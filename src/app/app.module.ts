1f872c3eddfb344b4916642410dadb0e8b8c5db0
Rajab Mkomwa <rajeyw@gmail.com>
2018-01-14 20:52:28 +0300

    added service to handle all http requests to dhis2 instance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import {HttpClientService} from './services/http-client.service';
+import {ManifestService} from './services/manifest.service';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,10 @@ import { environment } from '../environments/environment';
     BrowserModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [
+    ManifestService,
+    HttpClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
